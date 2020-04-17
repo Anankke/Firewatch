d207cdb5511d22c88737d36f48769d88a5d84f22
Anankke <anankke@protonmail.com>
2020-04-17 12:44:26 -0400

    init watcher

---
+++ main.py
@@ -0,0 +1,22 @@
+from pyrogram import Client
+from config import CONFIG
+
+Client.UPDATES_WORKERS = CONFIG.pyrogram.updates_workers
+
+
+def main():
+    app = Client(
+        "Firewatch",
+        api_id=CONFIG.pyrogram.api_id,
+        api_hash=CONFIG.pyrogram.api_hash,
+        app_version="Firewatch",
+        lang_code=CONFIG.pyrogram.lang_code,
+        bot_token=CONFIG.pyrogram.bot_token,
+        workers=CONFIG.pyrogram.workers,
+        plugins=dict(root="firewatch"),
+    )
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
